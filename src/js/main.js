34dedda35989beaea979c42b0c06389c50896bff
Vadim Vilkhivskiy <vadim.v.valki@gmail.com>
2022-08-04 23:24:04 +0300

    added file download

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import VideoPlayer from './modules/playVideo';
 import Difference from './modules/difference';
 import Form from './modules/form';
 import ShowInfo from './modules/showInfo'
+import Download from './modules/download'
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({btns: '.next', container: '.page'});
@@ -48,5 +49,7 @@ window.addEventListener('DOMContentLoaded', () => {
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
     new Form('.form').init();
 
-    new ShowInfo('.plus__content').init()
+    new ShowInfo('.plus__content').init();
+
+    new Download('.download').init();
 });
--- src/js/modules/showInfo.js
+++ src/js/modules/showInfo.js
@@ -1,12 +1,16 @@
-export default class ShowInfo{
-    constructor(triggers) {
+export default class ShowInfo {
+    constructor (triggers) {
         this.btns = document.querySelectorAll(triggers);
     }
+
     init() {
         this.btns.forEach(btn => {
             btn.addEventListener('click', () => {
-                btn.closest('.module__info-show').nextElementSibling.style.display = 'block';
-            })
-        })
+                const sibling = btn.closest('.module__info-show').nextElementSibling;
+
+                sibling.classList.toggle('msg');
+                sibling.style.marginTop = '20px';
+            });
+        });
     }
 }
